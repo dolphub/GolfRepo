9ab40355abea3af8ba89f32048a4f6ac42ededaa
Randy Mikkelsaar <randy.mikkelsaar@gmail.com>
2013-03-08 17:46:21 -0500

    Implemented Message Callback Testing

--- GoFish/GoFishClient/MainWindow.xaml.cs
+++ GoFish/GoFishClient/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +12,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using GoFishLibrary;
+using System.ServiceModel;
+
+
 namespace GoFishClient
 {
     /// <summary>
@@ -20,9 +23,75 @@ namespace GoFishClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IMessageBoard msgBrd = null;
+        private string prefix = "";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+
+                ChannelFactory<IMessageBoard> channel = new ChannelFactory<IMessageBoard>(
+                    new NetTcpBinding(),
+                    new EndpointAddress("net.tcp://localhost:12000/MessageBoardLibrary/MessageBoard"));
+
+                msgBrd = channel.CreateChannel();
+                //msgBrd = new MessageBoard();
+                listMessages.ItemsSource = msgBrd.getAllMessages();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+        private void buttonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (textPost.Text != "")
+            {
+                try
+                {
+                    msgBrd.postMessage(prefix + textPost.Text);
+                    textPost.Clear();
+                    listMessages.ItemsSource = msgBrd.getAllMessages();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void buttonSet_Click(object sender, RoutedEventArgs e)
+        {
+            if (textAlias.Text != "")
+            {
+                try
+                {
+                    prefix = "[" + textAlias.Text + "] ";
+                    buttonSet.IsEnabled = textAlias.IsEnabled = false;
+                    buttonSubmit.IsEnabled = textPost.IsEnabled = listMessages.IsEnabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                listMessages.ItemsSource = msgBrd.getAllMessages();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
--- GoFish/GoFishService/Program.cs
+++ GoFish/GoFishService/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ServiceModel;
+using GoFishLibrary;
+
+
 namespace GoFishService
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ServiceHost servHost = null;
+            try
+            {
+                // Address 
+                servHost = new ServiceHost(typeof(MessageBoard),
+                new Uri("net.tcp://localhost:12000/MessageBoardLibrary/"));
+                // Service contract and binding
+                servHost.AddServiceEndpoint(typeof(IMessageBoard), new
+                NetTcpBinding(), "MessageBoard");
+                // Manage the service’s life cycle
+                servHost.Open();
+                Console.WriteLine("Service started. Press a key to quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                if (servHost != null)
+                    servHost.Close();
+            }
         }
     }
 }
