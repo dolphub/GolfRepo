73c90e5d8e6bc7b1ac3cfd69955d9b48fbad59a3
Randy Mikkelsaar <randy.mikkelsaar@gmail.com>
2013-03-10 04:15:06 -0400

    Testing Image Swapping

--- ThreeHoleGolf/GolfClient/MainWindow.xaml.cs
+++ ThreeHoleGolf/GolfClient/MainWindow.xaml.cs
@@ -23,35 +23,37 @@ namespace GolfClient
     /// </summary>
     public partial class MainWindow : Window
     {
-//        private IShoe shoe = null;
+        //        private IShoe shoe = null;
         private int numPlayers = 0;
         public MainWindow()
         {
             InitializeComponent();
 
-//            try
-//            {
 
-//                // configure the ABCs of using the cardsLibrary as a service
-//                ChannelFactory<IShoe> channel = new ChannelFactory<IShoe>(
-//                 new NetTcpBinding(),
-//                 new EndpointAddress("net.tcp://localhost:9000/GolfLibrary/Shoe"));
 
-//                shoe = channel.CreateChannel();
+            //            try
+            //            {
 
-//                // set upslider control(# of decks)
-//                sliderDecks.Minimum = 1;
-//                sliderDecks.Maximum = 10;
-//                sliderDecks.Value = shoe.NumDecks;
+            //                // configure the ABCs of using the cardsLibrary as a service
+            //                ChannelFactory<IShoe> channel = new ChannelFactory<IShoe>(
+            //                 new NetTcpBinding(),
+            //                 new EndpointAddress("net.tcp://localhost:9000/GolfLibrary/Shoe"));
 
-//                // initialize the card counts
-//                updateCardCounts();
+            //                shoe = channel.CreateChannel();
 
-//            }
-//            catch (Exception ex)
-//            {
-//                MessageBox.Show(ex.Message);
-//            }
+            //                // set upslider control(# of decks)
+            //                sliderDecks.Minimum = 1;
+            //                sliderDecks.Maximum = 10;
+            //                sliderDecks.Value = shoe.NumDecks;
+
+            //                // initialize the card counts
+            //                updateCardCounts();
+
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                MessageBox.Show(ex.Message);
+            //            }
         }
 
         protected void btn_clickTest(object sender, EventArgs e)
@@ -61,83 +63,91 @@ namespace GolfClient
 
         private void testTemplate_Click_1(object sender, RoutedEventArgs e)
         {
-                numPlayers++;
-                PlayerTemplate pt = new PlayerTemplate("TestPlayer " + numPlayers, numPlayers * 7);
-                this.PlayerGrid.Children.Add(pt);
+            numPlayers++;
+            PlayerTemplate pt = new PlayerTemplate("TestPlayer " + numPlayers, numPlayers * 7);
+            this.PlayerGrid.Children.Add(pt);
+
+
+            //(btn_card2.FindName("img_card2") as Image).Source = new BitmapImage(new Uri(@"Images\7ofClubs.png"));
             
             
+            
+
+            
+            
+            //var card1Img = btn_card1.Template.FindName("img_card1", ControlTemplate);
         }
 
-//        private void updateCardCounts()
-//        {
-//            txtHandCount.Text = lstCards.Items.Count.ToString();
-//            txtShoeCount.Text = shoe.NumCards.ToString();
-//        }
+        //        private void updateCardCounts()
+        //        {
+        //            txtHandCount.Text = lstCards.Items.Count.ToString();
+        //            txtShoeCount.Text = shoe.NumCards.ToString();
+        //        }
 
-//        private void btnClose_Click(object sender, RoutedEventArgs e)
-//        {
-//            this.Close();
-//        }
+        //        private void btnClose_Click(object sender, RoutedEventArgs e)
+        //        {
+        //            this.Close();
+        //        }
 
-//        private void btnDraw_Click(object sender, RoutedEventArgs e)
-//        {
-//            try
-//            {
-//                // get a card from the shoe and add it to the hand
-//                string card = shoe.Draw();
-//                lstCards.Items.Add(card);
-//                updateCardCounts();
-//            }
-//            catch (Exception ex)
-//            {
-//                MessageBox.Show(ex.Message);
-//            }
-//        }
+        //        private void btnDraw_Click(object sender, RoutedEventArgs e)
+        //        {
+        //            try
+        //            {
+        //                // get a card from the shoe and add it to the hand
+        //                string card = shoe.Draw();
+        //                lstCards.Items.Add(card);
+        //                updateCardCounts();
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                MessageBox.Show(ex.Message);
+        //            }
+        //        }
 
-//        private void btnShuffle_Click(object sender, RoutedEventArgs e)
-//        {
-//            try
-//            {
-//                // shuffle the shoe
-//                shoe.Shuffle();
+        //        private void btnShuffle_Click(object sender, RoutedEventArgs e)
+        //        {
+        //            try
+        //            {
+        //                // shuffle the shoe
+        //                shoe.Shuffle();
 
-//                // reset listbox and card counts
-//                lstCards.Items.Clear();
-//                updateCardCounts();
-//            }
-//            catch (Exception ex)
-//            {
-//                MessageBox.Show(ex.Message);
-//            }
-//        }
+        //                // reset listbox and card counts
+        //                lstCards.Items.Clear();
+        //                updateCardCounts();
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                MessageBox.Show(ex.Message);
+        //            }
+        //        }
 
-//        private void sliderDecks_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-//        {
-//            try
-//            {
-//                if (shoe != null)
-//                {
-//                    // set the number of decks
-//                    shoe.NumDecks = (int)(sliderDecks.Value);
-//                    if (shoe.NumDecks == 1)
-//                        txtDeckCount.Text = "1 Deck";
-//                    else
-//                        txtDeckCount.Text = shoe.NumDecks + " Decks";
+        //        private void sliderDecks_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //        {
+        //            try
+        //            {
+        //                if (shoe != null)
+        //                {
+        //                    // set the number of decks
+        //                    shoe.NumDecks = (int)(sliderDecks.Value);
+        //                    if (shoe.NumDecks == 1)
+        //                        txtDeckCount.Text = "1 Deck";
+        //                    else
+        //                        txtDeckCount.Text = shoe.NumDecks + " Decks";
 
-//                    // reset the listbox and card counts
-//                    lstCards.Items.Clear();
-//                    updateCardCounts();
-//                }
-//            }
-//            catch (Exception ex)
-//            {
-//                MessageBox.Show("Slider error" + ex.Message);
-//            }
-//        }
+        //                    // reset the listbox and card counts
+        //                    lstCards.Items.Clear();
+        //                    updateCardCounts();
+        //                }
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                MessageBox.Show("Slider error" + ex.Message);
+        //            }
+        //        }
 
-//        private void btn_drawnCard_Click(object sender, RoutedEventArgs e)
-//        {
+        //        private void btn_drawnCard_Click(object sender, RoutedEventArgs e)
+        //        {
 
-//        }
+        //        }
     }
 }
